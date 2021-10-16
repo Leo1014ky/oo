533ceea8ba0794b0788d71a0f0b9a93c52946ed2
Leo1014ky <92613989+Leo1014ky@users.noreply.github.com>
2021-10-16 10:54:02 +0900

    Initial commit

---
+++ sketch.js
@@ -0,0 +1,42 @@
+let video
+let cl;
+function preload() {
+  cl = ml5.imageClassifier("https://teachablemachine.withgoogle.com/models/r2IXgzVRi/model.json")
+}
+
+function setup() {
+  createCanvas(window.innerWidth, window.innerHeight);
+  video = createCapture(VIDEO)
+  video.hide();
+  setTimeout(classify, 5000);
+
+}
+function classify() {
+  cl.classify(video, result)
+
+}
+
+
+function result(e, r) {
+  open("병3.html", "_self")
+  console.log(r);
+  const 종류 = r[0].label;
+  if(종류==="찰과상"){
+    open("병1.html", _self)
+  }else if(종류==="화상"){
+    open("병1.html", _self)
+  }else if(종류==="타박상"){
+    open("병1.html", _self)
+  }  
+
+}
+
+
+
+
+
+
+function draw() {
+  background(220);
+  image(video, 0, 0, window.innerWidth, window.innerHeight)
+}
